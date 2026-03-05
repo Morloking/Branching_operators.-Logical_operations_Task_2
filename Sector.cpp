79a48e23616b51f8356ff7f13827c8b38b5d374c
Morloking <dyubenkosergei@yandex.ru>
2026-03-05 04:23:56 +0300

    Add files via upload

---
+++ Sector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    int x{ 0 }; int y{ 0 }; int z{ 0 }; 
+    int max{ 0 }; int min{ 0 }; int middle{ 0 };
+    cout << "¬ведите первое число: ";
+    cin >> x;
+    cout << "¬ведите второе число : ";
+    cin >> y;
+    cout << "¬ведите третье число: ";
+    cin >> z;
+    cout << "\n–езультат: ";
+    min = x < y ? 
+        (x<z ? x:z) :
+         (y<z ? y:z);
+    max = x > y ?
+        (x>z ? x:z) :
+        (y>z ? y:z);
+    middle = (x != max && x != min) ?
+        x :
+        (y != max && y != min) ?
+        y : z;
+    cout << max << " " << middle << " " << min << "\n";
+}
